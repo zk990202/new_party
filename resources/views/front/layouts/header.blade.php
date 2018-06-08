1f39ed4ede0aeeed03c2e96649e158d5c6986297
zhangkai <zhangkai@twt.edu.com>
2018-06-08 15:58:59 +0800

    fix header link bug

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -79,57 +79,57 @@
         </a>
     </div>
     <div class="child-list">
-        <p><a href="index.blade.php">首页</a></p>
+        <p><a href="{{ url('/') }}">首页</a></p>
         <p><a href="#">网上党校</a></p>
         <ul>
-            <li><a href="ProposerTrain.html">申请人培训</a></li>
-            <li><a href="ActivistTrain.html">积极分子培训</a></li>
-            <li><a href="Preliminary-memeberTrain.html">预备党员培训</a></li>
+            <li><a href="{{ url('applicant/courseStudy') }}">申请人培训</a></li>
+            <li><a href="{{ url('academy/courseStudy') }}">积极分子培训</a></li>
+            <li><a href="{{ url('probationary/notice') }}">预备党员培训</a></li>
         </ul>
 
         <p><a href="#">我的支部</a></p>
         <ul>
-            <li><a href="Ownness.html">个人状态</a></li>
-            <li><a href="detial.html">支部详情</a></li>
-            <li><a href="member-list.html">支部成员列表</a></li>
-            <li><a href="#">我的学习小组</a></li>
-            <li><a href="PartyMembershipApplication.html">上传文献查看</a></li>
-            <li><a href="MyNews.html">我的消息</a></li>
-            <li><a href="#">我的申诉</a></li>
+            <li><a href="{{ url('personal/status') }}">个人状态</a></li>
+            <li><a href="{{ url('personal/partyBranch') }}">支部详情</a></li>
+            <li><a href="{{ url('personal/members') }}">支部成员列表</a></li>
+            <li><a href="{{ url('personal/groupMembers') }}">我的学习小组</a></li>
+            <li><a href="{{ url('personal/fileWatch/1/1') }}">上传文献查看</a></li>
+            <li><a href="{{ url('personal/myMessage/received') }}">我的消息</a></li>
+            <li><a href="{{ url('personal/myComplain') }}">我的申诉</a></li>
         </ul>
 
         <p><a href="#">通知公告</a></p>
         <ul>
-            <li><a href="ApplicantTraining.html">申请人培训</a></li>
-            <li><a href="InstituteActivistTrain.html">院积极分子培训</a></li>
-            <li><a href="ProbationaryPartyMember.html">预备党员培训</a></li>
-            <li><a href="PartyBranchSecretary.html">党支部书记培训</a></li>
-            <li><a href="ActiveNotice.html">活动通知</a></li>
+            <li><a href="{{ url('notification/applicant') }}">申请人培训</a></li>
+            <li><a href="{{ url('notification/academy') }}">院积极分子培训</a></li>
+            <li><a href="{{ url('notification/probationary') }}">预备党员培训</a></li>
+            <li><a href="{{ url('notification/secretary') }}">党支部书记培训</a></li>
+            <li><a href="{{ url('notification/activity') }}">活动通知</a></li>
         </ul>
 
         <p><a href="#">党建专项</a></p>
         <ul>
-            <li><a href="Hero.html">身边的英雄</a></li>
-            <li><a href="CentralSpirit.html">中央精神</a></li>
-            <li><a href="MassLine.html">群众路线</a></li>
-            <li><a href="ChineseDream.html">中国梦</a></li>
+            <li><a href="{{ url('partyBuildSpecial/hero') }}">身边的英雄</a></li>
+            <li><a href="{{ url('partyBuildSpecial/spirit') }}">中央精神</a></li>
+            <li><a href="{{ url('partyBuildSpecial/massLine') }}">群众路线</a></li>
+            <li><a href="{{ url('partyBuildSpecial/ChinaDream') }}">中国梦</a></li>
         </ul>
 
         <p><a href="#">党建培训</a></p>
         <ul>
-            <li><a href="PressCenter.html">新闻中心</a></li>
+            <li><a href="{{ url('news/partySchool') }}">新闻中心</a></li>
         </ul>
 
         <p><a href="#">重要文件</a></p>
         <ul>
-            <li><a href="ImportantFiles.html">规章制度</a></li>
-            <li><a href="RegularlyFile.html">常用文书</a></li>
-            <li><a href="MustRead.html">入党必读</a></li>
-            <li><a href="SystemBook.html">系统手册</a></li>
+            <li><a href="{{ url('commonFiles/regulation') }}">规章制度</a></li>
+            <li><a href="{{ url('commonFiles/instrument') }}">常用文书</a></li>
+            <li><a href="{{ url('commonFiles/mustRead') }}">入党必读</a></li>
+            <li><a href="{{ url('commonFiles/manual') }}">系统手册</a></li>
         </ul>
         <p><a href="#">理论学习</a></p>
         <ul>
-            <li><a href="TheoryStudy.html">理论经典</a></li>
+            <li><a href="{{ url('theoryStudy') }}">理论经典</a></li>
         </ul>
 
         <p><a href="#">支部分采</a></p>
